4bfe8e5ffc3e214660e37d85353825707b6e53b1
Misha <159485186+zhyzhiijr17@users.noreply.github.com>
2024-11-04 20:00:40 +0200

    commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿class Fruit
+{
+    public string Name { get; set; }
+
+    public Fruit(string name)
+    {
+        Name = name;
+    }
+}
+
+class Apple : Fruit
+{
+    public string Variety { get; set; }
+    public string Color { get; set; }
+
+    public Apple(string name, string variety, string color) : base(name)
+    {
+        Variety = variety;
+        Color = color;
+    }
+}
+
+class Pear : Fruit
+{
+    public string Color { get; set; }
+
+    public Pear(string name, string color) : base(name)
+    {
+        Color = color;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Apple apple = new Apple("Apple", "Granny Smith", "Green");
+        Pear pear = new Pear("Pear", "Yellow");
+
+        Console.WriteLine($"Fruit: {apple.Name}, Variety: {apple.Variety}, Color: {apple.Color}");
+        Console.WriteLine($"Fruit: {pear.Name}, Color: {pear.Color}");
+    }
+}
